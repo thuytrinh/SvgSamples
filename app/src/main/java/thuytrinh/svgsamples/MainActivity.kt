573472664e2b74ce876593e2f63475c1a5b98f5f
Thuy Trinh <thuy.ntrinh@gmail.com>
2018-10-06 16:43:36 +0200

    Initial commit

---
+++ app/src/main/java/thuytrinh/svgsamples/MainActivity.kt
@@ -0,0 +1,26 @@
+package thuytrinh.svgsamples
+
+import android.graphics.drawable.PictureDrawable
+import android.net.Uri
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    val svgFileName = "android.svg"
+    val svgUri = Uri.parse("file:///android_asset/$svgFileName")
+    Glide.with(this)
+      .`as`(PictureDrawable::class.java)
+      .transition(DrawableTransitionOptions.withCrossFade())
+      .listener(SvgSoftwareLayerSetter())
+      .load(svgUri)
+      .into(svgImageView)
+  }
+}
---
+++ app/src/main/java/thuytrinh/svgsamples/SvgModule.java
@@ -0,0 +1,35 @@
+package thuytrinh.svgsamples;
+
+import android.content.Context;
+import android.graphics.drawable.PictureDrawable;
+import android.support.annotation.NonNull;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+import com.caverock.androidsvg.SVG;
+
+import java.io.InputStream;
+
+/**
+ * Module for the SVG sample app.
+ * Note that, as of Glide 4.8.0, we need to keep this class as Java instead of Kotlin.
+ */
+@GlideModule
+public class SvgModule extends AppGlideModule {
+  @Override
+  public void registerComponents(
+    @NonNull Context context, @NonNull Glide glide,
+    @NonNull Registry registry
+  ) {
+    registry.register(SVG.class, PictureDrawable.class, new SvgDrawableTranscoder())
+      .append(InputStream.class, SVG.class, new SvgDecoder());
+  }
+
+  // Disable manifest parsing to avoid adding similar modules twice.
+  @Override
+  public boolean isManifestParsingEnabled() {
+    return false;
+  }
+}
