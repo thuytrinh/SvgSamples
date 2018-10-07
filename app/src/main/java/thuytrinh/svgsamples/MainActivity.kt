fb9124af36b4b203255848f19f26520b32a0970c
Thuy Trinh <thuy.ntrinh@gmail.com>
2018-10-07 14:08:41 +0200

    Use AndroidSVG to show SVG file

--- app/src/main/java/thuytrinh/svgsamples/MainActivity.kt
+++ app/src/main/java/thuytrinh/svgsamples/MainActivity.kt
@@ -8,19 +8,23 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import kotlinx.android.synthetic.main.activity_main.*
 
-
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
     val svgFileName = "android.svg"
+
+    // Show the SVG file by Glide.
     val svgUri = Uri.parse("file:///android_asset/$svgFileName")
     Glide.with(this)
       .`as`(PictureDrawable::class.java)
       .transition(DrawableTransitionOptions.withCrossFade())
       .listener(SvgSoftwareLayerSetter())
       .load(svgUri)
-      .into(svgImageView)
+      .into(glideSvgImageView)
+
+    // Show the SVG file by SVGImageView.
+    androidSvgImageView.setImageAsset(svgFileName)
   }
 }
