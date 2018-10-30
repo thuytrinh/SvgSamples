653f504eb9bb299b54c3d9282442aea3441b5c91
Thuy Trinh <thuy.ntrinh@gmail.com>
2018-10-30 17:45:51 +0100

    Initially create ViewPagerBindings

---
+++ ViewPagerBindings/src/main/java/thuytrinh/viewpagerbindings/ItemsActivity.kt
@@ -0,0 +1,11 @@
+package thuytrinh.viewpagerbindings
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ItemsActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_items)
+  }
+}
